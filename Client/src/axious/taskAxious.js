623fcf1e8f2bae11182c5864a0128eac8f871e23
Mahesh792 <mawhesjung143@gmail.com>
2024-06-23 15:00:21 +1000

    updated api url

--- Client/src/axious/taskAxious.js
+++ Client/src/axious/taskAxious.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.PROD
+  ? "https://ntdl-full-stack-c88d.onrender.com/api/tasks"
+  : "http://localhost:8000/api/tasks";
 console.log(API_URL);
 
 // get a task |GET |READ
